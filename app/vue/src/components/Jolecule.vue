807b72cd6b02638750a6e083c3722a28fbacdc16
Bosco Ho <bosco.ho@kelvininc.com>
2021-07-08 15:09:57 +1000

    improved css of app

--- app/vue/src/components/Jolecule.vue
+++ app/vue/src/components/Jolecule.vue
@@ -1,38 +1,33 @@
 <template lang="pug">
 .d-flex.flex-row(style="height: calc(100vh);")
-  .overflow-auto(style="width: 250px; border-right: 1px solid #999;")
-    .header Download PDB
-    .ms-3
-      input.view-text(type='text' v-model='pdbId' style='width: 90px')
-      .d-inline.ps-2.jolecule-small-button(
-        @click='loadFromPdbId()'
-        style='width: 2em'
-      ) FETCH
-      br(clear='all')
-      span {{ error }}
-    .header
-      | Local files
-    .view-card.view-text.pointer(v-for='(file, j) in drawer.directories' :key="j + 'f'" @click='openDir(drawer.dirname, file)')
-      i.fa.fa-folder
-      | {{ file }}
-    .view-card.view-text.pointer(v-for='(file, k) in drawer.files' :key="k + 'e'" @click='openFileReplace(file)')
-      | [
-      span(style='text-transform: uppercase') {{ file.name }}
-      | ]
-      | {{ file.title }}
+  #file-column.p-2.d-flex.flex-column
+    .header PDB
+    .d-flex.flex-row.flex-wrap.align-items-center
+      .input-group.input-group-sm(style='width: 90px')
+        input.form-control.m-0.view-text(type='text' v-model='pdbId')
+      .jolecule-small-button.m-0(@click='loadFromPdbId()')
+        | fetch
+    span(v-if="error") {{ error }}
+    .header Files
+    .overflow-auto.pe-2
+      .view-card.view-text.pointer(v-for='(file, j) in drawer.directories' :key="j + 'f'" @click='openDir(drawer.dirname, file)')
+        i.fa.fa-folder
+        | {{ file }}
+      .view-card.view-text.pointer(v-for='(file, k) in drawer.files' :key="k + 'e'" @click='openFileReplace(file)')
+        span(style='text-transform: uppercase') {{ file.name }}
+        span.file-title {{ " - " + file.title }}
   #jolecule-container
-  .d-flex.flex-column(style="border-left: 2px solid #aaa;")
-    .header
-      | Views
-      .mt-2
-        .d-inline.jolecule-small-button(@click='createView') Save
-    .ps-2.pe-2.overflow-scroll(style="width: 300px;")
+  #view-column.p-2.d-flex.flex-column
+    .header Views
+    div
+      .d-inline.jolecule-small-button(@click='createView') save
+    .mt-2.overflow-scroll.pe-2
       .view-card(
         :class="{'active-border': view.isSelect}"
         v-for='(view, i) in views' :key='i'
       )
         template(v-if="view.isEdit")
-          textarea.view-text.view-textarea(v-model="view.editText" rows="6")
+          textarea.view-text.p-2.w-100(v-model="view.editText" rows="6")
           .mt-2.d-flex.flex-row
             .jolecule-small-button(@click='saveEditText(view.id)') save
             .jolecule-small-button.ms-2(@click='editOffView(view.id)') discard
@@ -41,11 +36,11 @@
             span {{ view.head }}{{ " " }}
             span(v-html='view.text')
           span.jolecule-author {{view.creator}}
-          .mt-2.d-flex.flex-row.justify-content-between
+          .mt-2.d-flex.flex-row.justify-content-between(v-if="i > 0")
             .d-flex.flex-row
-              .jolecule-small-button(@click='editView(view.id)') edit
-              .jolecule-small-button.ms-2(@click='swapUp(view.id)') &uarr;
-              .jolecule-small-button.ms-2(@click='swapDown(view.id)') &darr;
+              .jolecule-small-button( @click='editView(view.id)') edit
+              .jolecule-small-button.ms-2(v-if="i > 1" @click='swapUp(view.id)') &uarr;
+              .jolecule-small-button.ms-2(v-if="i < views.length - 1" @click='swapDown(view.id)') &darr;
             .jolecule-small-button.ms-2(@click='deleteView(view.id)') x
 </template>
 
@@ -56,13 +51,11 @@ body {
 }
 .header {
   padding-top: 12px;
-  padding-left: 20px;
-  padding-bottom: 12px;
   text-align: left;
-  font-size: 14px;
-  letter-spacing: 0.2em;
-  color: #999;
   font-family: Helvetica, sans-serif;
+  font-size: 14px;
+  letter-spacing: 0.12em;
+  color: #999;
   text-transform: uppercase;
 }
 .active-border {
@@ -72,14 +65,13 @@ body {
   min-height: 0;
   min-width: 0;
   flex: 1;
-  display: flex;
   margin: 0;
   padding: 0;
 }
 .view-card {
   padding: 7px;
   box-sizing: border-box;
-  margin: 0px 20px 5px 5px;
+  margin-bottom: 3px;
   border-radius: 3px;
   border: 1px solid #bbb;
   background-color: #bbb;
@@ -95,9 +87,24 @@ body {
   background-color: #bbb;
   color: #333;
 }
-.view-textarea {
-  width: 100%;
-  padding: 5px;
+#file-column {
+  width: 250px;
+  border-right: 1px solid #AAA;
+}
+#view-column {
+  border-left: 2px solid #aaa;
+  width: 300px;
+}
+@media (max-width: 992px) {
+  .file-title {
+    display: none
+  }
+  #file-column {
+    width: 140px;
+  }
+  #view-column {
+    width: 200px;
+  }
 }
 </style>
 
