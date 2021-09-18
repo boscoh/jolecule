81e14077da18a7141904ec8a6cf6d056828e0435
Bosco Ho <apposite@gmail.com>
2021-09-18 12:18:10 +1000

    reordered examples

---
+++ examples/alignments/prep-aquaria.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+function wrapJson (wrapJsFname, jsonText) {
+  let wrappedText = `define(function() {
+    
+return ${jsonText}
+
+});`
+  fs.writeFileSync(wrapJsFname, wrappedText)
+}
+
+let fnames = [
+  'P04637.4qo1.B.json',
+  'P04637.3q05.A.json',
+  'P04637.features.json',
+  'P04637.5mg7.json'
+]
+for (let fname of fnames) {
+  let data = require('./' + fname)
+  wrapJson('./' + fname + '.js', JSON.stringify(data, null, 2))
+}
